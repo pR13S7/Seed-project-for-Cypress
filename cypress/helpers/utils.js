0a4d8a2492816d2f807d811b35d799c5db8578e9
Vitaly Andreev <anghbad@gmail.com>
2021-09-30 10:30:08 +0300

    add some helper functions to utils.js
    - new version in package.json
    - add some helper functions to utils.js

--- cypress/helpers/utils.js
+++ cypress/helpers/utils.js
@@ -1,3 +1,32 @@
 let faker = require('faker')
 const { _ } = Cypress
 
+//==== grep data from cells in table to an array
+export const cellsToNamesObjects = (cells) => {
+    return _.map(cells, (cell) => {        
+        return cell.textContent
+    })
+} 
+
+//===== check if array is sorted by alphabet
+export const stringArrIsSorted = (stringArr, isReverse) =>{
+    let sortedStringArr = [...stringArr]
+    sortedStringArr.sort((a, b) => a !== b ? a < b ? -1 : 1 : 0)            
+
+    if(isReverse){
+        sortedStringArr = sortedStringArr.reverse()
+    }
+
+    expect(stringArr,'String array are sorted 📈').to.deep.equal(sortedStringArr)    
+}
+
+//==== No Operation function to use in callbacks
+export const noop = () =>{}
+
+//==== Returns a random number between min (inclusive) and max (inclusive)
+export const randomInRange = (edgeA = 0, edgeB = 100) =>{
+    edgeA = Math.ceil(edgeA);
+    edgeB = Math.floor(edgeB)
+
+    return Math.floor(Math.random() * (edgeA - edgeB + 1)) + edgeA
+}
